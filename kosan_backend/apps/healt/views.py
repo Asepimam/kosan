aa575ae16b4a8dee94bda33987bc719caae91038
Asepimam <asepimam329@gmail.com>
2025-12-02 22:12:47 +0700

    Add health check API view to monitor application status

---
+++ kosan_backend/apps/healt/views.py
@@ -0,0 +1,18 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+
+class CheckHealthView(APIView):
+    """
+    View to check the health status of the application.
+    """
+
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        """
+        Return a simple health status response.
+        """
+        return Response(
+            {"status":"ok","message": "Application is healthy"}, status=status.HTTP_200_OK) 
